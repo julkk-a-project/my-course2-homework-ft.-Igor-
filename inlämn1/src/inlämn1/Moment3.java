b4e598abf1c4dce0aca04f464fa55449a5801fa8
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2018-11-08 10:29:53 +0200

    qwd

---
+++ inlämn1/src/inlämn1/Moment3.java
@@ -0,0 +1,41 @@
+package inlämn1;
+
+import javax.swing.JOptionPane;
+
+public class Moment3 {
+	public static double getWeight(){
+		double weight = Double.parseDouble(JOptionPane.showInputDialog(null, "insert your weigth in kg." ));
+		return weight;
+	}
+	public static double getLength(){
+		double length = Double.parseDouble(JOptionPane.showInputDialog(null, "insert your length in cm"));
+		double powLength = Math.pow((length / 100), 2);
+		return powLength;
+	}
+	public static double getBmi(){
+		double weight = getWeight();
+		double length = getLength();
+		double bmi = Math.round((weight / length) * 10d) / 10d;
+		return bmi;
+	}
+	public static String sort(double bmi) {
+		if (bmi < 18.5) {
+			return "Undervikt";
+		}else if (bmi < 25) {
+			return "Normalvikt";
+		}else if (bmi < 30) {
+			return "Övervikt";
+		}else {
+			return "Fetma";
+		}
+	
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double bmi = getBmi();
+		String sortedBmi = sort(bmi);
+		JOptionPane.showMessageDialog(null, "BMI: " + bmi + "\nViktklass: " + sortedBmi);
+		
+		
+	}
+}
