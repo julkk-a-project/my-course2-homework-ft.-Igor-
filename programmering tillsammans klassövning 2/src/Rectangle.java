49ecb18d63cfce7e04f52190d7b04bbb9e088374
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2018-11-13 20:01:22 +0200

    Update programmering tillsammans klassövning 2/src/Rectangle.java

---
+++ programmering tillsammans klassövning 2/src/Rectangle.java
@@ -0,0 +1,22 @@
+
+public class Rectangle {
+
+	int width;
+	int height;
+	
+	//functions
+	int getArea() {
+		return width*height;
+	}
+	//constructor
+	Rectangle (int w, int h) {
+		width = w;
+		height = h;
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
