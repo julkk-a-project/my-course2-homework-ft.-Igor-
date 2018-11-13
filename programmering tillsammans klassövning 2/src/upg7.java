3e4caa5675018a214346a3a1738cfac0da6d73d8
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2018-11-13 23:47:31 +0200

    wow
    xd

---
+++ programmering tillsammans klassövning 2/src/upg7.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class upg7 {
+
+	public static void writer(String Sline) {
+		String [] line = Sline.split("\\|");
+		try {
+			FileWriter writer = new FileWriter("C:\\bob\\enNyTextFil.txt");
+			BufferedWriter buf = new BufferedWriter(writer);
+			for (int i = 0;i<3;i++) {
+				buf.write(line[i]);
+				buf.newLine();
+			}
+			buf.close();
+		} catch (IOException e) {
+			System.out.println("WRITEFAULT");
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			FileReader reader = new FileReader("C:\\enTextFil.txt");
+			BufferedReader buf = new BufferedReader(reader);
+			String line;
+			String Sline = "";
+			while ((line = buf.readLine()) != null) {
+				System.out.print(line+"\n");
+				Sline += (line+"|");
+			}
+			buf.close();
+			writer(Sline);
+		} catch (IOException e) {
+			//buf.close();
+			
+		}
+
+
+	}
+
+}
