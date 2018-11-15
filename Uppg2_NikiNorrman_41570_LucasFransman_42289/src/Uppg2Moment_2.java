6506640c0621fe2ddf5e80bed86372abe02607ef
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2018-11-15 18:11:39 +0200

    moment2 done

--- Uppg2_NikiNorrman_41570_LucasFransman_42289/src/Uppg2Moment_2.java
+++ Uppg2_NikiNorrman_41570_LucasFransman_42289/src/Uppg2Moment_2.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class Uppg2Moment_2 {
@@ -12,18 +14,22 @@ public class Uppg2Moment_2 {
 		int compChoise = (int)(Math.random() * 10 + 1);
 		int playerChoise = getInt("Chose a number that you want the computer to guess!\nhint: the computer will only guess numbers from 1 to 10 :))))");
 		String playerClaim = "!";
-		while (playerClaim != "=") {
+		while (!playerClaim.equals("=")) {
 			playerClaim = "!";
 			System.out.println(playerClaim);
 			while (!playerClaim.equals("<") && !playerClaim.equals(">") && !playerClaim.equals("=")) {
 				playerClaim = JOptionPane.showInputDialog("The computer guessed "+compChoise+".\nWas it >, < or = your choise?");
 			}
 			System.out.println("lol");
-			if (playerClaim == ">") {
-				compChoise = (int)(Math.random() * 10 + compChoise);
+			if (playerClaim.equals(">")) {
+				System.out.println("lollol");
+				compChoise = (int)(Math.random() * (10 - compChoise)) + compChoise+1;
 			}
-			else if (playerClaim == "<") {
-				
+			else if (playerClaim.equals("<")) {
+				compChoise = (int)(Math.random() * (compChoise - 1)) + 1;
+			}
+			else if (playerClaim.equals("=")) {
+				JOptionPane.showMessageDialog(null, "computer: 'Yay! i am good at this game!!!\nYour original number was "+playerChoise+" and you let the computer win by it saying "+compChoise+".", "Winrar", 1);
 			}
 		}
 		JOptionPane.showMessageDialog(null, "Correct! the anserw was "+compChoise+"!", "Yass", 1);
