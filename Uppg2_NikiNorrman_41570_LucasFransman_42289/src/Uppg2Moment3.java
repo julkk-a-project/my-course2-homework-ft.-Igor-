51564967f72b73c6a634da6e6ec8360a6cba5e59
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2018-11-19 21:31:24 +0200

    mom3

---
+++ Uppg2_NikiNorrman_41570_LucasFransman_42289/src/Uppg2Moment3.java
@@ -0,0 +1,52 @@
+import javax.swing.JOptionPane;
+
+public class Uppg2Moment3 {
+
+		public static int getInt(String string) {
+			String SInt = JOptionPane.showInputDialog(string);
+			int amount = Integer.parseInt(SInt);
+			return amount;
+		}
+		public static int minihandler(int x, int mini) {
+			if (x > mini) {
+				return x;
+			} else {
+				return mini;
+			}
+		}
+		public static int maxihandler(int x, int maxi) {
+			if (x < maxi) {
+				return x;
+			} else {
+				return maxi;
+			}
+		}
+		
+		public static void main(String[] args) {
+			int compChoise = (int)5;
+			int playerChoise = getInt("Chose a number between 1 and 10 that you want the computer to guess!");
+			int mini = 1;
+			int maxi = 10;
+			String playerClaim = "!";
+			while (!playerClaim.equals("=")) {
+				playerClaim = "!";
+				System.out.println(playerClaim);
+				while (!playerClaim.equals("<") && !playerClaim.equals(">") && !playerClaim.equals("=")) {
+					playerClaim = JOptionPane.showInputDialog("The computer guessed "+compChoise+".\nIs "+compChoise+" <, > or = your choise?");
+				}
+				if (playerClaim.equals("<")) {
+					mini = minihandler(compChoise, mini);
+					compChoise = (mini+maxi)/2;
+				}
+				else if (playerClaim.equals(">")) {
+					maxi = maxihandler(compChoise, maxi);
+					compChoise = (maxi+mini)/2;
+				}
+				else if (playerClaim.equals("=")) {
+					JOptionPane.showMessageDialog(null, "computer: 'Yay! i am good at this game!!!\n\nYour original number was "+playerChoise+" and you let the computer win by it saying "+compChoise+".", "Winrar", 1);
+				}
+			}
+			//JOptionPane.showMessageDialog(null, "Correct! the anserw was "+compChoise+"!", "Yass", 1);
+		}
+
+}
