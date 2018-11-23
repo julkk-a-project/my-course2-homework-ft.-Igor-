84fcfeabf0d1e81136bc57e9394e998125aa65fa
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2018-11-23 20:18:40 +0200

    update to §2

--- Uppg2_NikiNorrman_41570_LucasFransman_42289/src/Uppg2Moment_2.java
+++ Uppg2_NikiNorrman_41570_LucasFransman_42289/src/Uppg2Moment_2.java
@@ -6,19 +6,32 @@ public class Uppg2Moment_2 {
 
 	public static int getInt(String string) {
 		String SInt = JOptionPane.showInputDialog(string);
-		int amount = Integer.parseInt(SInt);
-		return amount;
+		int amount = -1;
+		try {
+			amount = Integer.parseInt(SInt);
+		} catch (NumberFormatException ex ) {
+			JOptionPane.showMessageDialog(null, "Try an integer...");
+			amount = getInt(string);
+		}
+		if (amount < 1 || amount > 10) {
+			JOptionPane.showMessageDialog(null, "Try a value between 1 and 10...");
+			return getInt(string);
+		} else {
+			return amount;
+		}
 	}
 	
 	public static void main(String[] args) {
 		int compChoise = (int)(Math.random() * 10 + 1);
-		int playerChoise = getInt("Chose a number between 1 and 10 that you want the computer to guess!");
+		int playerChoise = getInt("Chose a number between 1 and 10 that you want the computer to guess!"); //Flavourtext. just cosmetic, to make the player feel better about this little game.
+		int times = 0;
 		String playerClaim = "!";
 		while (!playerClaim.equals("=")) {
 			playerClaim = "!";
 			while (!playerClaim.equals("<") && !playerClaim.equals(">") && !playerClaim.equals("=")) {
 				playerClaim = JOptionPane.showInputDialog("The computer guessed "+compChoise+".\nWas it <, > or = your choise?");
 			}
+			times ++;
 			if (playerClaim.equals("<")) {
 				compChoise = (int)(Math.random() * (10 - compChoise)) + compChoise+1;
 			}
@@ -26,10 +39,11 @@ public class Uppg2Moment_2 {
 				compChoise = (int)(Math.random() * (compChoise - 1)) + 1;
 			}
 			else if (playerClaim.equals("=")) {
-				JOptionPane.showMessageDialog(null, "computer: 'Yay! i am good at this game!!!\nYour original number was "+playerChoise+" and you let the computer win by it saying "+compChoise+".", "Winrar", 1);
+				JOptionPane.showMessageDialog(null, "computer: 'Yay! i am good at this game!!!\nYour original number was "+
+													playerChoise+" and you let the computer win by it saying "+compChoise+
+													".\nThis took the computer "+times+" tries.", "Winrar", 1);
 			}
 		}
-		JOptionPane.showMessageDialog(null, "Correct! the anserw was "+compChoise+"!", "Yass", 1);
 
 	}
 }
