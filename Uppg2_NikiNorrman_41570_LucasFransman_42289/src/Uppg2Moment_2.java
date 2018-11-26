752840091c728f8d5e372b201549a96ec6bf699e
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2018-11-26 14:34:33 +0200

    final fixes to upg2 WOOHOO

--- Uppg2_NikiNorrman_41570_LucasFransman_42289/src/Uppg2Moment_2.java
+++ Uppg2_NikiNorrman_41570_LucasFransman_42289/src/Uppg2Moment_2.java
@@ -1,10 +1,8 @@
-import java.util.Random;
-
 import javax.swing.JOptionPane;
 
 public class Uppg2Moment_2 {
 
-	public static int getInt(String string) {
+	public static int getInt(String string) { //legacy code
 		String SInt = JOptionPane.showInputDialog(string);
 		int amount = -1;
 		try {
@@ -22,23 +20,29 @@ public class Uppg2Moment_2 {
 	}
 	
 	public static void main(String[] args) {
-		int compChoise = (int)(Math.random() * 10 + 1);
+		int compChoise = (int)(Math.random() * 10 + 1); //first guess
 		int playerChoise = getInt("Chose a number between 1 and 10 that you want the computer to guess!"); //Flavourtext. just cosmetic, to make the player feel better about this little game.
-		int times = 0;
+		int times = 0; //to count amount of tries.
 		String playerClaim = "!";
 		while (!playerClaim.equals("=")) {
-			playerClaim = "!";
-			while (!playerClaim.equals("<") && !playerClaim.equals(">") && !playerClaim.equals("=")) {
+			playerClaim = "!"; //to reset variable playerClaim.
+			while (!playerClaim.equals("<") && !playerClaim.equals(">") && !playerClaim.equals("=")) { //Require user to type correctly.
 				playerClaim = JOptionPane.showInputDialog("The computer guessed "+compChoise+".\nWas it <, > or = your choise?");
 			}
 			times ++;
-			if (playerClaim.equals("<")) {
+			
+			//
+			// Notice, computer has a very short term memory. it only remembers it's last guess. this could be fixed with a "mini" and "maxi" variable, as i did in §3 and §4.
+			// ...but as you said face to face, §2 isn't meant to be effective.
+			//
+			
+			if (playerClaim.equals("<")) { //handels < event
 				compChoise = (int)(Math.random() * (10 - compChoise)) + compChoise+1;
 			}
-			else if (playerClaim.equals(">")) {
+			else if (playerClaim.equals(">")) { //handels > event
 				compChoise = (int)(Math.random() * (compChoise - 1)) + 1;
 			}
-			else if (playerClaim.equals("=")) {
+			else if (playerClaim.equals("=")) { //handels = event
 				JOptionPane.showMessageDialog(null, "computer: 'Yay! i am good at this game!!!\nYour original number was "+
 													playerChoise+" and you let the computer win by it saying "+compChoise+
 													".\nThis took the computer "+times+" tries.", "Winrar", 1);
--- Uppg2_NikiNorrman_41570_LucasFransman_42289/src/Uppg2Moment_3.java
+++ Uppg2_NikiNorrman_41570_LucasFransman_42289/src/Uppg2Moment_3.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class Uppg2Moment_3 {
 
-	public static int getInt(String string) {
+	public static int getInt(String string) { //legacy code
 		String SInt = JOptionPane.showInputDialog(string);
 		int amount = -1;
 		try {
@@ -18,14 +18,14 @@ public class Uppg2Moment_3 {
 			return amount;
 		}
 	}
-		public static int minihandler(int x, int mini) {
+		public static int minihandler(int x, int mini) { //prevents mini from becoming smaller it was before.
 			if (x > mini) {
 				return x;
 			} else {
 				return mini;
 			}
 		}
-		public static int maxihandler(int x, int maxi) {
+		public static int maxihandler(int x, int maxi) { //prevents maxi becoming larger than it was before.
 			if (x < maxi) {
 				return x;
 			} else {
@@ -34,25 +34,25 @@ public class Uppg2Moment_3 {
 		}
 		
 		public static void main(String[] args) {
-			int compChoise = 5;
-			int playerChoise = getInt("Chose a number between 1 and 10 that you want the computer to guess!"); //Flavourtext. just cosmetic, to make the player feel better about this little game.
+			int compChoise = 5; //initial guess is 5, to minimise total amount of guesses
+			int playerChoise = getInt("Chose a number between 1 and 10 that you want the computer to guess!"); //Flavour text. just cosmetic, to make the player feel better about this little game.
 			int mini = 1;
-			int maxi = 10;
+			int maxi = 11;
 			int times = 0;
 			String playerClaim = "!";
 			while (!playerClaim.equals("=")) {
-				playerClaim = "!";
-				while (!playerClaim.equals("<") && !playerClaim.equals(">") && !playerClaim.equals("=")) {
+				playerClaim = "!"; //to reset variable playerClaim.
+				while (!playerClaim.equals("<") && !playerClaim.equals(">") && !playerClaim.equals("=")) { //Require user to type correctly.
 					playerClaim = JOptionPane.showInputDialog("The computer guessed "+compChoise+".\nIs "+compChoise+" <, > or = your choise?");
 				}
 				times ++;
 				if (playerClaim.equals("<")) {
 					mini = minihandler(compChoise, mini);
-					compChoise = (mini+maxi)/2;
+					compChoise = (mini+maxi)/2; //takes average (int) of mini and maxi.
 				}
 				else if (playerClaim.equals(">")) {
 					maxi = maxihandler(compChoise, maxi);
-					compChoise = (maxi+mini)/2;
+					compChoise = (maxi+mini)/2; //takes average (int) of mini and maxi.
 				}
 				else if (playerClaim.equals("=")) {
 					JOptionPane.showMessageDialog(null, "computer: 'Yay! i am good at this game!!!\n\nYour original number was "+playerChoise+
